e86325e594a592430572fa66b087eb876aeaf5b9
D7703_23 <d7703_23@D7703-23ui-iMac.local>
2018-03-28 12:55:09 +0900

    clear

--- UITextField Delegate01/ViewController.swift
+++ UITextField Delegate01/ViewController.swift
@@ -1,25 +1,49 @@
 //
 //  ViewController.swift
-//  UITextField Delegate01
+//  UITextField Delegate 01
 //
-//  Created by D7703_23 on 2018. 3. 22..
-//  Copyright © 2018년 D7703_23. All rights reserved.
+//  Created by D7703_15 on 2018. 3. 22..
+//  Copyright © 2018년 D7703_15. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate{
+    
+    @IBOutlet weak var txt: UILabel!
+    @IBOutlet weak var txtField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //Delegate와 ViewController 연결
+        txtField.delegate = self
+        
+        txtField.placeholder = "입력을 하세요"
+        txtField.clearButtonMode = UITextFieldViewMode.whileEditing //클리어 버튼
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func bt(_ sender: Any) {
+        txt.text = " Hello " + txtField.text!
+        txtField.text = ""
+        txtField.resignFirstResponder()
+        
     }
-
-
+    //view를 터치하면 키패드가 내려감
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        txtField.resignFirstResponder()
+        view.backgroundColor = UIColor.green
+    }
+    // UITextFieldDelegate Mathod
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool{
+        textField.resignFirstResponder()
+        view.backgroundColor = UIColor.brown
+        return true
+    }
+    func textFieldShouldClear(_ textField: UITextField)
+        -> Bool {
+            textField.backgroundColor = UIColor.gray
+            return true
+    }
 }
 
